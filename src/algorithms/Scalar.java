11c16bddc8cefededa2bdffde08543623dc1efc6
pierrick <deshayes.pierrick@hotmail.fr>
2016-12-29 23:25:56 +0100

    src/algorithms/Scalar.java

--- src/algorithms/Scalar.java
+++ src/algorithms/Scalar.java
@@ -11,8 +11,8 @@ public class Scalar {
 	public static List<Solution> run(int nbObj, Solution randomSol) {
 		List<Solution> list = new ArrayList<Solution>();
 //		OffLineFilter off = new OffLineFilter();
-		for (int i = 0; i < nbObj; i++) {
-			Solution s = LocalSearch.run(randomSol, i);
+		for (int n = 0; n < nbObj; n++) {
+			Solution s = LocalSearch.run(randomSol, n);
 			list.add(s);
 		}
 //		off.filter(archive);
